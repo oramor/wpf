8e2f5d5c63c63cf202ffbe44e9f399f983f05bd0
Roman Rodin <romaro83@gmail.com>
2023-04-20 15:14:06 +0300

    Test

---
+++ TestApp_VSM/Controls/CustomButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TestApp_VSM
+{
+    class CustomButton : Button
+    {
+        readonly static Color _defaultColor = Color.FromArgb(255, 0, 255, 0);
+
+        static CustomButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
+        }
+
+        //public CustomButton()
+        //{
+        //    var color = Color.FromArgb(255, 255, 255, 0);
+        //    BackgroundMouseOver = new SolidColorBrush(color);
+        //}
+
+        #region BackgroundMouseOver
+
+        public SolidColorBrush BackgroundMouseOver
+        {
+            get => (SolidColorBrush)GetValue(BackgroundMouseOverProperty);
+            set => SetValue(BackgroundMouseOverProperty, value);
+        }
+
+        public static readonly DependencyProperty BackgroundMouseOverProperty = DependencyProperty.Register(
+            nameof(BackgroundMouseOver),
+            typeof(SolidColorBrush),
+            typeof(CustomButton),
+            new PropertyMetadata(new SolidColorBrush(_defaultColor))
+          );
+
+        #endregion
+    }
+}
---
+++ TestApp_VSM/Converters/SolidColorBrushToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TestApp_VSM
+{
+    public class SolidColorBrushToColorConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (value is SolidColorBrush b)
+            {
+                return Color.FromArgb(b.Color.A, b.Color.R, b.Color.G, b.Color.B);
+            }
+
+            Type type = value.GetType();
+            throw new InvalidOperationException("Unsupported type [" + type.Name + "]");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

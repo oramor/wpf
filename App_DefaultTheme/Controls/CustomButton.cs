1fe34aa35c15e712a36e113d4e7769e7b9c30385
Roman R <romaro83@gmail.com>
2023-04-22 02:41:16 +0300

    App_DefaultTheme

---
+++ App_DefaultTheme/Controls/CustomButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace App_DefaultTheme.Controls
+{
+    public class CustomButton : Control
+    {
+        static CustomButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
+        }
+    }
+}

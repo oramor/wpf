182732d55f729aa6f32ede6f621bc0775cf3ab7f
Roman Rodin <romaro83@gmail.com>
2025-07-11 03:18:11 +0300

    fix WpfExceptionHandling

--- WpfExceptionHandling/Program.cs
+++ WpfExceptionHandling/Program.cs
@@ -7,6 +7,8 @@ namespace WpfExceptionHandling
         [STAThread]
         public static void Main()
         {
+            MessageBox.Show("Приложение будет запущено после закрытия этого окна", "Application_DispatcherUnhandledException", MessageBoxButton.OK, MessageBoxImage.Information);
+
             AppDomain.CurrentDomain.UnhandledException += AppDomain_UnhandledException;
 
             var vm = new MainVM();
@@ -23,10 +25,12 @@ namespace WpfExceptionHandling
 
         private static void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
+            // Здесь только логируем ошибку
+
             MessageBox.Show(e.Exception.Message, "Application_DispatcherUnhandledException", MessageBoxButton.OK, MessageBoxImage.Error);
 
             // В этом случае исключение не попадет в AppDomain_UnhandledException
-            e.Handled = true;
+            //e.Handled = true;
         }
 
         private static void AppDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -35,6 +39,8 @@ namespace WpfExceptionHandling
                 return;
 
             MessageBox.Show(exception.Message, "AppDomain_UnhandledException", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            Application.Current.Shutdown();
         }
     }
 }
